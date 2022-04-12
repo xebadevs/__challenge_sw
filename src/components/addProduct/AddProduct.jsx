c113e35f3904555df3495c7c2119d9d7d3b3efc4
Sebastian Londero <xebadevs@gmail.com>
2022-04-11 23:53:38 -0300

    cancel_button

--- src/components/addProduct/AddProduct.jsx
+++ src/components/addProduct/AddProduct.jsx
@@ -1,13 +1,18 @@
 import Form from "./Form"
+import { useNavigate } from "react-router-dom"
+
 
 export default function AddProduct() {
+
+    const navigate = useNavigate()
+
     return (
         <div className='container'>
             <section className='title-section'>
                 <h1>Product Add</h1>
                 <div>
                     <button>Save</button>
-                    <button id='delete-product-btn'>Cancel</button>
+                    <button id='delete-product-btn' onClick={() => navigate('/')}>Cancel</button>
                 </div>
             </section>    
             <hr />
